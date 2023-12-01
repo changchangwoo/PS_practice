b45d54ec51a686b49c14bf88f34a8836689ccb94
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2023-12-01 19:43:52 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,13 @@
+function solution(left, right) {
+    var answer = 0;
+    for(let i = left ; i <= right; i++) {
+        let count = 0
+        for(let j = 1; j <= i; j++) {
+            if(i%j===0) count +=1  
+        }
+        if(count % 2 === 0) answer += i
+        else if (count % 2 === 1) answer -= i
+    }
+
+    return answer;
+}
