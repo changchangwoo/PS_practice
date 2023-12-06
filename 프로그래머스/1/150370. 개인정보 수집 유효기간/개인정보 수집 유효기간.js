a1156df191b4662b2ed8dc43d24644dacc4e9ab9
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2023-12-06 16:50:11 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 6.24 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,27 +1,45 @@
 function solution(today, terms, privacies) {
-    // today를 날짜 객체로 변환
-    const todayDate = new Date(today.replace(/\./g, '/'));
-
-    // 파기해야 할 개인정보 번호를 저장할 배열
-    const toDestroy = [];
-
-    // privacies 배열을 순회하며 개인정보를 확인
-    for (let i = 0; i < privacies.length; i++) {
-        const privacyInfo = privacies[i].split(' ');
-        const privacyDate = new Date(privacyInfo[0].replace(/\./g, '/'));
-
-        // 약관 종류와 유효기간 추출
-        const [termType, termDuration] = terms.find(term => term.startsWith(privacyInfo[1])).split(' ');
-
-        // 유효기간을 날짜로 변환하여 개인정보 보관 가능 여부 확인
-        const expirationDate = new Date(privacyDate);
-        expirationDate.setMonth(expirationDate.getMonth() + parseInt(termDuration));
-
-        // 개인정보 파기 여부 확인
-        if (expirationDate <= todayDate) {
-            toDestroy.push(i + 1); // 배열은 0부터 시작하므로 +1
+    let result = []
+    let hash = new Map()
+    today = today.split('.')
+    terms.forEach(element => {
+        element = element.split(' ')
+        hash.set(element[0], element[1])
+    });
+    privacies.forEach((element, i) => {
+        element = element.split(' ')
+        add_value = 0
+        privacies_date = element[0].split('.')
+        privacies_day = privacies_date[2]
+        privacies_month = privacies_date[1]
+        privacies_year = privacies_date[0]
+        add_year_flag = false
+        check_month = +privacies_month + +hash.get(element[1])
+        check_day = +privacies_day -1
+        if (check_month > 12) {
+            add_value = Math.floor(check_month/12)
+            check_month = check_month % 12
+            if(check_month === 0) {
+                check_month = 12
+                add_value -=1
+            }
+            add_year_flag = true
         }
-    }
-
-    return toDestroy;
+        if(check_day === 0) {
+            check_month -=1
+            check_day = 28
+        }
+        if (add_year_flag) check_years = +privacies_year + add_value
+        else {check_years = +privacies_year}
+        console.log(check_years, check_month, check_day)
+        // 비교
+        if(check_years < +today[0]) result.push(i+1)
+        else if(check_years === +today[0]) {
+            if(check_month < +today[1]) result.push(i+1)
+            else if(check_month === +today[1]) {
+                if(check_day < +today[2] ) result.push(i+1)
+            }
+        }
+    });
+    return result
 }
