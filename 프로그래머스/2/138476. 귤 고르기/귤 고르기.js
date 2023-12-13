a81a15bb19628a9d6b14050167ad10dc400ffbab
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2023-12-13 19:47:20 +0900

    [level 2] Title: 귤 고르기, Time: 700.28 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,24 @@
+function solution(k, tangerine) {
+    var answer = 0;
+    var hash = new Map()
+    tangerine.forEach(element => {
+        if (!hash.has(element)) {
+            hash.set(element, 1)
+        } else {
+            hash.set(element, hash.get(element) + 1)
+        }
+    });
+    var arr = Array.from(hash, ([key, value]) => ({ no: key, count: value }));
+    arr = arr.sort((a, b) => a.count - b.count)
+    var sort_tangerin = []
+    arr.forEach(element => {
+        for (let i = 0; i < element.count; i++) {
+            sort_tangerin.push(element.no)
+        }
+    });
+    for(let i = 0; i < tangerine.length -k; i++) {
+        sort_tangerin.shift()
+    }
+    sort_tangerin = new Set(sort_tangerin)
+    return sort_tangerin.size
+}
