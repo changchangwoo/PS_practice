c5db865974c82192319928d71f738c41c560ccb2
changchangwoo <changchangwoo@gmail.com>
2023-12-14 17:01:12 +0900

    백준 문제풀이 도우미 추가

---
+++ fs_input.js
@@ -0,0 +1,32 @@
+// oneline
+function oneline ()  {
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let num = Number(input)
+for(let i = 1; i <= num; i++){
+    console.log(i)
+}
+}
+// multline
+
+function multline () {
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let count = input[0]
+let numbers = [];
+
+for(let i = 1; i < input.length; i++) {
+    if(input[i] !== '') {
+        numbers.push(input[i].split(' '));
+    }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    let num1 = Number(numbers[i][0]);
+    let num2 = Number(numbers[i][1]);
+
+    console.log(num1 + num2)
+}
+}
+
+// 잘 풀었는데 출력형식이 이상하다고 할 때 :: console.log(result.replace(/\n/g, ""));
--- 프로그래머스/code.js
+++ 프로그래머스/code.js
@@ -1,25 +1,26 @@
-const dfs = (graph, v, vistied) => {
-    vistied[v] = true
-    console.log(v)
-
-    for (const cur of graph[v]) {
-        if (!visited[cur]) {
-            dfs(graph, cur, visited)
+function solution(k, tangerine) {
+    var answer = 0;
+    var hash = new Map()
+    tangerine.forEach(element => {
+        if (!hash.has(element)) {
+            hash.set(element, 1)
+        } else {
+            hash.set(element, hash.get(element) + 1)
         }
+    });
+    var arr = Array.from(hash, ([key, value]) => ({ no: key, count: value }));
+    arr = arr.sort((a, b) => a.count - b.count)
+    var sort_tangerin = []
+    arr.forEach(element => {
+        for (let i = 0; i < element.count; i++) {
+            sort_tangerin.push(element.no)
+        }
+    });
+    for(let i = 0; i < tangerine.length -k; i++) {
+        sort_tangerin.shift()
     }
+    sort_tangerin = new Set(sort_tangerin)
+    return sort_tangerin.size
 }
 
-let graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
-    [1, 7],
-]
-
-let visited = new Array(9).fill(false)
-dfs(graph, 1, visited)
+console.log(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
