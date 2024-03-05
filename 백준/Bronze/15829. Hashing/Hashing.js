d4a1d52a8767ea04b6b4e4646ce677bf44841083
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2024-03-05 15:01:47 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 9580 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -1,8 +1,14 @@
-const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n')
-const N = Number(input.shift())
-const arr = input.shift().split('')
-let sum = 0
-for(let i = 0; i < arr.length; i++) {
-    sum += (arr[i].charCodeAt()-96) * (Math.pow(31, i))
+const fs = require('fs');
+const [n, str] = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+const N = +n
+let sum = 0;
+let r = 1;
+for (let i = 0; i < N; i++) {
+    sum += (str.charCodeAt(i) - 96) * r
+    sum %= 1234567891;
+    r *= 31
+    r %= 1234567891;
 }
+
 console.log(sum)
