0c27447e3154f6efec574eb121f7c31bb30beeb9
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2024-03-16 13:39:28 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,26 @@
+const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n')
+const [N, M] = input.shift().split(' ').map(Number)
+const arr = input.shift().split(' ').map(Number).sort((a,b) => a-b)
+const result = []
+const answer = []
+const visited = new Array(N+1).fill(false)
+
+function recursion (num) {
+    if(result.length === M) {
+        answer.push(result.join(' '))
+        return
+    }
+    for(let i = 0; i < arr.length; i++) {
+        if(!visited[i] && arr[i] >= num) {
+            visited[i] = true
+            result.push(arr[i])
+            recursion(arr[i])
+            result.pop()
+            visited[i] = false
+        }
+    }
+}
+
+recursion(0)
+
+console.log(answer.join('\n'))
