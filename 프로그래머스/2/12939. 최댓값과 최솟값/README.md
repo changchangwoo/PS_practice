942b7122422d26ee0223c5f7dd3a1dcd9ed7b4c9
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2024-03-31 12:43:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.06 ms
+메모리: 33.6 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 19:9:14
+2024년 03월 31일 12:43:06
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,14 @@
 function solution(s) {
-    let answer = ''
-    s = s.split(' ')
-    answer += Math.min(...s) + ' ' + Math.max(...s)
-    return answer
+    var answer = '';
+    var strs = s.trim().split(' ')
+    let arr = []
+    for(let str of strs) {
+        if(str[0] === '-') {
+            str = str.slice(1)
+            arr.push(+str * -1)
+        }
+        else arr.push(+str)
+    }
+    arr = arr.sort((a,b) => a- b)
+    return [arr[0], arr[arr.length-1]].join(' ')
 }
