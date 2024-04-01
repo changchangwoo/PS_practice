44b7d287f125aa60d428a19a13eebccab70ed772
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2024-04-01 17:24:32 +0900

    [level 2] Title: 소수 찾기, Time: 0.54 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,16 +1,43 @@
-const getPermutations = function (arr, selectNumber) {
-    const results = [];
-    if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+/*
+종이 조각을 붙여 소수 개수 찾기
+numbers 1이상 7이하 => 백트래킹, 완전탐색
+numbers를 길이별로 탐색해서 값을 넣는다
+그 값을 가지고 소수를 판별
+*/
 
-    arr.forEach((fixed, index, origin) => {
-        const rest = [...origin.slice(0, index), ...origin.slice(index + 1)]; // 해당하는 fixed를 제외한 나머지 배열
-        const permutations = getPermutations(rest, selectNumber - 1); // 나머지에 대해 순열을 구한다.
-        const attached = permutations.map((permutation) => [fixed, ...permutation]); // 돌아온 순열에 대해 떼 놓은(fixed) 값 붙이기
-        results.push(...attached); // 배열 spread syntax 로 모두다 push
-    });
-
-    return results; // 결과 담긴 results return
-};
+function solution(numbers) {
+    let answer = []
+    const result = []
+    let count = 0
+    let visited = new Array(numbers.length).fill(false)
+    function dfs(num) {
+        if(result.length === num) {
+            answer.push(result.join(''))
+            return
+        }
+        for (let i = 0; i < numbers.length; i++ ) {
+            if(!visited[i]) {
+                visited[i] = true
+                result.push(numbers[i])
+                dfs(num)
+                visited[i] = false
+                result.pop()
+            }
+        }
+    }
+    for(let i = 1; i <= numbers.length; i++) {
+        dfs(i)
+    }
+    answer = answer.map(val => +val)
+    let set = new Set(answer)
+    for(let item of set) {
+        if(item < 2) continue
+        else {
+            if(isPrime(item)) count++;
+        }
+    }
+    return count
+}
 
 function isPrime(value) {
     for(var i = 2; i <= Math.sqrt(value); i++) {
@@ -19,26 +46,4 @@ function isPrime(value) {
         }
     }
     return true
-}
-
-function solution(numbers) {
-    var answer = 0;
-    let result = []
-    let p_arr = []
-    let set = new Set()
-    numbers = numbers.split('')
-    for (let i = 1; i <= numbers.length; i++) {
-        result = getPermutations(numbers, i)
-        p_arr.push(result)
-    }
-    p_arr.forEach(element => {
-        element.forEach(e => {
-            set.add(+e.join(''))
-        });
-    });
-    for(value of set) {
-        if(value === 0 || value === 1) continue
-        if(isPrime(value)) answer++
-    }
-    return answer;
 }
