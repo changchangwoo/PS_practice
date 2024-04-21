9ab8b519f7782a19688fa24d01500445f011be06
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2024-04-21 18:10:37 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.66 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,9 +1,9 @@
-function solution(A, B) {
-    var answer = 0;
-    A = A.sort((a, b) => a - b)
-    B = B.sort((a, b) => b - a)
-    for(let i = 0; i < A.length; i++) {
-        answer = answer +(A[i] * B[i])
+function solution(A,B){
+    let arrA = A.sort((a,b) => a-b)
+    let arrB = B.sort((a,b) => b-a)
+    let sum = 0;
+    for(let i = 0; i < arrA.length ; i++) {
+        sum += arrA[i]*arrB[i]
     }
-    return answer;
+    return sum;
 }
