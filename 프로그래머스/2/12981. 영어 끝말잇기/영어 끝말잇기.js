7f7c562413bf9386ee4064515e08c98c2d6b2d8c
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-01-03 22:54:29 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,28 +1,37 @@
+// 해쉬에 값을 넣고 해당 값이 있는지 확인, 그리고 그 값에 따라 중복 단어 체크
+// 탈락되는 순간 인덱스를 기준으로 n값으로 나눈 값, 몇 번째 나머지 연산자 0일경우 = n번쨰
 function solution(n, words) {
-    var answer = [];
-    firstAlpha = words[0][0]
-    lastAlpha = words[0][words[0].length-1]
-    const primaryCheck = [words[0]]
-    let flag = 0
-    let idx = 0
-    words.shift()
-    let count = 1
-    for(let word of words) {
-        count++
-        if(count > n) {
-            idx++
-            count =1
-        }
-        if(word[0] !== lastAlpha || primaryCheck.includes(word)) {
-            flag = 1
-            break
-        } else {
-            primaryCheck.push(word)
-            lastAlpha = word[word.length-1]
-        }
-
+  const hash = new Map();
+  const stack = []
+  var answer
+  let index = 1;
+  for(let word of words) {
+    if(stack.length > 0) { // 단어의 처음과 마지막을 찾는 로직
+      let lastWords = stack[stack.length-1]
+      let lastWord = lastWords.split("").pop();
+      let startWord = word.split("").shift();
+      if(lastWord !== startWord) {
+        answer = failCheck(index, n)
+        break;
+      }
     }
-    if( flag === 0) return [0,0]
-    else return [count, idx+1]
+    stack.push(word)
 
+    if(hash.get(word) === true) { // 단어의 중복을 찾는 로직
+      answer = failCheck(index, n)
+      break;
+    } else {
+      hash.set(word, true)
+    }
+    index++;
+  }
+
+  if(!answer) answer = [0, 0]
+  return answer;
+}
+
+function failCheck(index, n) {
+  let cycle = Math.ceil(index / n)
+  let num = (index % n === 0 ? n : index % n)
+  return [num, cycle] 
 }
