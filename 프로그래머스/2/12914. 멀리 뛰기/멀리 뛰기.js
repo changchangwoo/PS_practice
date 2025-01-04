e358030b92b074f7c95ce4d0f3a193883532aaa5
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-01-04 17:02:33 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,10 +1,11 @@
 function solution(n) {
-    const dp =[]
-    var answer = 0;
-    dp[1] = 1
-    dp[2] = 2
-    for(let i = 3; i < n+1; i++) {
-        dp[i] = (dp[i-1] + dp[i - 2]) % 1234567
+    if(n === 1) return 1
+    if(n === 2) return 2
+    const DP = new Array(n+1).fill(0)
+    DP[1] = 1;
+    DP[2] = 2;
+    for(let i = 3; i <=n; i++) {
+        DP[i] = (DP[i-1]+DP[i-2])% 1234567
     }
-    return dp[n];
+    return DP[n] 
 }
