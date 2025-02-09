5c02e8aff971b0043d495a25c4120d606f4c3a0a
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-02-09 13:47:01 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.04 ms
+메모리: 33.5 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 15:02:50
+2025년 02월 09일 13:46:43
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,29 +1,79 @@
 /*
-절댓값 10^n을 기준으로 0이 되는 값을 찾아야한다
-한번 탐색할때마다 result값이 1씩 증가한다
-뒤에부터 값을 탐색해야할까?
-+를 해서 다음 수에 맞추는 경우 앞자리가 1이 증가한다 << 5인 경우에는 내리는게 유리
-1,2,3,4,5 내림 6,7,8,9 올림
+-1, +1, -10, +10, -100, +100, 10^
+엘리베이터 버튼을 누르면 현재 층 이동
+
+그리디하게 접근?
+마지막수를 0으로 맞춘다
+
+마지막수가 5보다 큰 경우 더해서
+5보다 작거나 같은 경우 뺴서
+
+1, 2, 3 , 4
+1, 2, 3, 4, 5
+1, 2, 3, 4, 5
+1, 2
+
+485
+11
+
+2554
+
+4
+5
+4
+3
+
+
+
+만약에 타겟값이 5인경우 prev 값을 보고 판단
+
+
+
 */
 
 function solution(storey) {
     var answer = 0;
-    while(storey > 0) {
-        let restStorey = storey % 10
-        let prevStorey = storey % 100
-        if(restStorey > 5) {
-            answer += 10-restStorey
-            storey += 10-restStorey
-        } else {
-            if(restStorey === 5 && prevStorey !== restStorey && prevStorey > 50) {
-                answer += 10-restStorey
-                storey += 10-restStorey
-            } else {
-            answer += restStorey
-            storey -= restStorey
+    let storyArr = storey.toString().split('')
+    let upperCase = false
+    while(true) {
+        let target = Number(storyArr.pop());
+        if(upperCase) target+=1
+        if(storyArr.length === 0) { // 마지막 케이스 경우
+            if(target <= 5) {
+                answer += target
+                break;
+            } else if (target > 5) {
+                answer += (10-target) + 1
+                break;
+            }
         }
-    }
-        storey = storey/10
+        let prev = Number(storyArr[storyArr.length-1]) 
+        upperCase = false
+        while(true) {
+            if(target === 0 || target === 10) {
+                if(target === 10) upperCase = true
+                break;
+            }
+            if(target < 5) {
+                target--;
+                answer++;
+            } else if (target > 5) {
+                target++;
+                answer++;
+            } else if (target === 5) {
+                if(prev >= 5) {
+                    target++;
+                    answer++;
+                } else {
+                    target--
+                    answer++;
+                }
+
+            }
+        }
+        }
+    if(upperCase === true) {
+
     }
     return answer;
 }
