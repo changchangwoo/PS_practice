1e03f0c6df2b654edf4b14cc23fc7c82de613df6
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-02-18 16:17:25 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 108 ms, Memory: 9392 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.js
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.js
@@ -1,45 +1,46 @@
-const input = require('fs').readFileSync("./dev/stdin").toString().trim().split('\n');
-const [N, M] = input.shift().split(' ').map(Number);
-const graph = [];
-const dp = new Array(101).fill(Infinity);
-for (let i = 1; i <= 100; i++) {
-    graph[i] = i;
-}
+/*
+지문에 대한 생각
+1. 항상 앞선 수가 최소 주사위의 수인가?
+=> 해당하는 경우라면 중복방문 필요 X
+=> 뭔가 그런 것 같은데 왜 그런지 확답을 못내리겠음
 
-const starts = [];
-const ends = [];
+*/
+const input = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input.shift().split(" ").map(Number);
+const graph = [];
+const visited = new Array(101).fill(false);
+const dp = new Array(101).fill(Infinity);
+for (let i = 0; i <= 100; i++) {
+  graph.push(i);
+}
 input.map((item) => {
-    let [start, end] = item.split(' ').map(Number);
-    starts.push(start);
-    ends.push(end);
-    graph[start] = end;
+  const [start, end] = item.split(" ").map(Number);
+  graph[start] = end;
 });
 
 const bfs = () => {
-    const queue = [];
-    queue.push([1, 0]);
-    dp[1] = 0;
-
-    while (queue.length > 0) {
-        let [current, diceCount] = queue.shift();
-
-
-        for (let i = 1; i <= 6; i++) {
-            let next = current + i;
-            if (next > 100) continue;
-
-
-            let destination = graph[next];
-
-
-            if (dp[destination] > diceCount + 1) {
-                dp[destination] = diceCount + 1;
-                queue.push([destination, diceCount + 1]);
-            }
-        }
+  const queue = [];
+  queue.push([1, 1]);
+  visited[1] = true;
+  while (queue.length > 0) {
+    let [curX, diceCount] = queue.shift();
+    for (let i = 1; i <= 6; i++) {
+      let next = curX + i;
+      if (next > 100) break;
+      if (!visited[next]) {
+        visited[next] = true;
+        queue.push([graph[next], diceCount + 1]);
+        dp[next] = diceCount;
+      }
     }
+  }
 };
 
 bfs();
 
-console.log(dp[100]); 
+console.log(dp[100]);
