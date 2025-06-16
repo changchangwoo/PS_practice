bc9cbff48a251afe183451ae4a407beeda661b7e
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-06-16 17:31:36 +0900

    [Silver III] Title: 카드게임, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2621. 카드게임/카드게임.js
+++ 백준/Silver/2621. 카드게임/카드게임.js
@@ -1,128 +1,95 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-let colors = []
-let numbers = []
-let answer = []
-let nothingCheck = 0
+const checkPattern = (arr) => {
+  const patternArr = [];
+  const numArr = [];
+  const setPattern = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    const [pattern, num] = arr[i].split(" ");
+    patternArr.push(pattern);
+    numArr.push(Number(num));
+    setPattern.add(pattern);
+  }
+  const sortedNumArr = numArr.sort((a, b) => a - b);
 
-for (let i = 0; i < input.length; i++) {
-    input[i] = input[i].split(' ')
-    colors.push(input[i][0])
-    numbers.push(+input[i][1])
-}
-
-answer.push(checkRoyalandFlush(colors, numbers))
-answer.push(checkFourAndFull(numbers))
-
-let tempNumbers = [...numbers].sort()
-if (checkStraight(tempNumbers)) {
-    answer.push(Math.max(...numbers) + 500)
-}
-answer.push(checkTripleAndTwo(numbers))
-answer.push(checkOne(numbers))
-
-for(let i = 0; i < answer.length; i++) {
-    nothingCheck += answer[i]
-}
-
-if(nothingCheck === 0) answer.push(Math.max(...numbers) + 100)
-
-console.log(Math.max(...answer))
-
-// 숫자와 문자 담은 배열
-function checkRoyalandFlush(colors, numbers) {
-    let setColors = new Set(colors)
-    let tempNumbers = [...numbers].sort()
-    if (setColors.size === 1) { // 플러시인 경우
-        if (checkStraight(tempNumbers)) { // 스트레이트 플러시
-            return Math.max(...numbers) + 900
-        } else return Math.max(...numbers) + 600
-    } else return 0
-}
-
-function checkFourAndFull(numbers) {
-    let setNumbers = new Set(numbers)
-    let tempNumbers = [...numbers]
-    if (setNumbers.size === 2) {
-        let hash = new Map()
-        for (let i = 0; i < tempNumbers.length; i++) {
-            if (hash.has(tempNumbers[i])) {
-                hash.set(tempNumbers[i], hash.get(tempNumbers[i]) + 1);
-            } else {
-                hash.set(tempNumbers[i], 1);
-            }
-        } // 해시를 통해 더 큰 수 탐색
-        hash = [...hash]
-        if (hash[0][1] === 4 || hash[1][1] === 4) { // 포카드인 경우
-            let four = hash[0][1] > hash[1][1] ? hash[0][0] : hash[1][0]
-            return four + 800
-        } else if (hash[0][1] === 3 || hash[1][1] === 3) { // 풀하우스 인 경우
-            let triple = hash[0][1] > hash[1][1] ? hash[0][0] : hash[1][0]
-            let double = hash[0][1] < hash[1][1] ? hash[0][0] : hash[1][0]
-            return (triple * 10 + double + 700)
-        }
-    } else return 0
-}
-
-function checkStraight(numbers) {
-    let startCheck = numbers[0]
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] !== startCheck) return false
-        startCheck += 1
+  if (setPattern.size === 1) {
+    let flag = true;
+    for (let i = 1; i < 5; i++) {
+      if (sortedNumArr[i - 1] + 1 === sortedNumArr[i]) continue;
+      else {
+        flag = false;
+        break;
+      }
     }
-    return true
-}
+    return flag ? 900 + sortedNumArr[4] : 600 + sortedNumArr[4];
+  } else {
+    return 0;
+  }
+};
 
-function checkTripleAndTwo(numbers) {
-    let setNumbers = new Set(numbers)
-    if (setNumbers.size === 3) {
-        let hash = new Map()
-        for (let i = 0; i < tempNumbers.length; i++) {
-            if (hash.has(tempNumbers[i])) {
-                hash.set(tempNumbers[i], hash.get(tempNumbers[i]) + 1);
-            } else {
-                hash.set(tempNumbers[i], 1);
-            }
-        }
-        hash = [...hash]
-        if (hash[0][1] === 3 || hash[1][1] === 3 || hash[2][1] === 3) // 트리플인 경우 
-        {
-            let triple = 0
-            for(let i = 0; i < hash.length; i++) {
-                if(hash[i][1] === 3) triple = hash[i][0]
-            }
-            return triple+400
-        }
-        else if (hash[0][1] === 2 || hash[1][1] === 2 || hash[2][1] === 2) // 투페어인 경우 
-        {
-            let tempArr = []
-            for(let i = 0; i < hash.length; i++) {
-                if(hash[i][1] === 2) tempArr.push(hash[i][0])
-            }
-            return (Math.max(...tempArr) * 10 + Math.min(...tempArr) + 300)
-        }
+const checkNumber = (arr) => {
+  const numArr = [];
+  const setNum = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    const [pattern, num] = arr[i].split(" ");
+    numArr.push(Number(num));
+    setNum.add(Number(num));
+  }
+  if (setNum.size === 2) {
+    const [sm, lg] = [...setNum].sort((a, b) => a - b);
+    let count = 0;
+    for (let i = 0; i < numArr.length; i++) {
+      if (numArr[i] === sm) count++;
     }
-    else return 0
-}
+    if (count === 1) return 800 + lg;
+    else if (count === 4) return 800 + sm;
+    else if (count === 2) return lg * 10 + sm + 700;
+    else if (count === 3) return sm * 10 + lg + 700;
+  }
 
-function checkOne(numbers) {
-    let setNumbers = new Set(numbers)
-    if(setNumbers.size === 4) {
-        let hash = new Map()
-        for (let i = 0; i < tempNumbers.length; i++) {
-            if (hash.has(tempNumbers[i])) {
-                hash.set(tempNumbers[i], hash.get(tempNumbers[i]) + 1);
-            } else {
-                hash.set(tempNumbers[i], 1);
-            }
-        }
-        hash = [...hash]
-        let two = 0
-        for(let i = 0; i < hash.length; i++) {
-            if(hash[i][1] === 2) two = hash[i][0] 
-        }        
-        return two + 200
-    } else return 0
+  const sortedNumArr = numArr.sort((a, b) => a - b);
+  let flag = true;
+  for (let i = 1; i < 5; i++) {
+    if (sortedNumArr[i - 1] + 1 === sortedNumArr[i]) continue;
+    else {
+      flag = false;
+      break;
+    }
+  }
+  if (flag === true) return 500 + sortedNumArr[4];
 
-}
+  if (setNum.size === 3) {
+    const [sm, md, lg] = [...setNum].sort((a, b) => a - b);
+    let [smCount, mdCount, lgCount] = [0, 0, 0];
+    for (let i = 0; i < numArr.length; i++) {
+      if (numArr[i] === sm) smCount++;
+      if (numArr[i] === md) mdCount++;
+      if (numArr[i] === lg) lgCount++;
+    }
+    if (smCount === 3) return sm + 400;
+    if (mdCount === 3) return md + 400;
+    if (lgCount === 3) return lg + 400;
+
+    if (smCount === 1) return lg * 10 + md + 300;
+    if (mdCount === 1) return lg * 10 + sm + 300;
+    if (lgCount === 1) return md * 10 + sm + 300;
+  }
+
+  if (setNum.size === 4) {
+    const setArr = [...setNum];
+    for (let i = 0; i < setArr.length; i++) {
+      const arr = numArr.filter((el) => el === setArr[i]);
+      if (arr.length === 2) return 200 + setArr[i];
+    }
+  }
+
+  return sortedNumArr[4] + 100;
+};
+
+const arr = input.map((item) => item.trim());
+console.log(Math.max(checkPattern(arr), checkNumber(arr)));
