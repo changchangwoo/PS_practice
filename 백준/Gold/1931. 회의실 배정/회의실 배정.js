520e062006788f9ac71094ae03004d3bbbe7319d
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-10-13 21:57:28 +0900

    [Gold V] Title: 회의실 배정, Time: 336 ms, Memory: 43616 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.js
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -1,22 +1,22 @@
-const input = require("fs").readFileSync("./dev/stdin").toString().split("\n");
+const input = require('fs').readFileSync("./dev/stdin").toString().trim().split('\n')
 const N = Number(input.shift());
-const arr = input.map((el) => el.split(" ").map((el) => Number(el)));
-let answer = 1;
-
-const sortArr = arr.sort((a, b) => {
-  if (a[1] !== b[1]) return a[1] - b[1];
-  else {
-    return a[0] - b[0];
-  }
-});
-
-curTime = sortArr[0][1];
-
-for (let i = 1; i < N; i++) {
-  if (sortArr[i][0] >= curTime) {
-    curTime = sortArr[i][1];
-    answer++;
-  }
+const arr = input.map((item) => item.split(' ').map(Number)).sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0]
+    else return a[1] - b[1]
 }
+)
 
-console.log(answer);
+
+let prev_end = -1;
+let answer = 0;
+
+arr.forEach(([start, end]) => {
+    if (start >= prev_end) {
+        answer++;
+        prev_end = end
+        // console.log(`in- ${start}, ${end}`)
+    }
+})
+
+
+console.log(answer)
