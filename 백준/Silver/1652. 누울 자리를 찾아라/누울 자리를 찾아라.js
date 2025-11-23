1e7c908e640f1b806a8f80d4407824ebdebe510f
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-11-23 18:32:32 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 92 ms, Memory: 9440 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.js
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.js
@@ -1,54 +1,59 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-let N = +input[0]
-let arr = []
-for (let i = 1; i <= N; i++) {
-    arr.push(input[i])
+/*
+1. 연속해서 두 칸 이상의 빈 칸 존재시 누울 수 있음
+2. 가로 가능 세로 가능
+3. 눕게 된다면 쭉 뻗어서 반드시 벽에 닿게 됨
+4. 단순 구현 => 가로 -1, +1 하나씩 찾아서 눕도록
+*/
+
+let input = require('fs').readFileSync("./dev/stdin").toString().trim().split('\n')
+const N = Number(input.shift());
+for (let i = 0; i < N; i++) {
+    input[i] = input[i].trim().split('')
 }
 
-let verticalCount = 0
-let horizonCount = 0
-
-// 가로 횟수
-for (let i = 0; i < arr.length; i++) {
+let rowCount = 0;
+let colCount = 0;
+for (let i = 0; i < N; i++) {
+    let rowFlag = false
     let count = 0;
-    let flag = true;
-
-    for (let j = 0; j < arr.length; j++) {
-        if (arr[i][j] === '.') {
-            if (!flag) continue
+    for (let j = 0; j < N; j++) {
+        if (input[i][j] === '.') {
+            if (rowFlag) {
+                continue;
+            }
             count++;
-        } else if (arr[i][j] === 'X') {
-            flag = true
-            count = 0
+            if (count >= 2) {
+                rowFlag = true
+                rowCount++;
+            }
         }
-        if (count === 2) {
-            verticalCount++;
+        else {
             count = 0;
-            flag = false
-
+            rowFlag = false
         }
     }
 }
 
-// 세로 횟수
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < N; i++) {
+    let colFlag = false
     let count = 0;
-    let flag = true;
-    for (let j = 0; j < arr.length; j++) {
-        if (arr[j][i] === '.') {
-            if (!flag) continue
+    for (let j = 0; j < N; j++) {
+        if (input[j][i] === '.') {
+            if (colFlag) {
+                continue;
+            }
             count++;
-        } else if (arr[j][i] === 'X') {
-            flag = true
-            count = 0
+            if (count >= 2) {
+                colFlag = true
+                colCount++;
+            }
         }
-        if (count === 2) {
-            horizonCount++;
+        else {
             count = 0;
-            flag = false
+            colFlag = false
         }
     }
 }
 
-console.log(verticalCount, horizonCount)
+console.log(rowCount, colCount)
+
