fb106669bbf881666b216dec7e8c3cb7c98439ba
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-11-25 15:49:18 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 12304 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,49 +1,42 @@
-let input = require("fs")
-  .readFileSync("./dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
-
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-const N = Number(input.shift());
-
-const BFS = (startX, startY, arr, visited, M, N) => {
-  const queue = [];
-  queue.push([startX, startY]);
-  visited[startX][startY] = true;
-  while (queue.length > 0) {
-    const [cx, cy] = queue.shift();
-    for (let i = 0; i < 4; i++) {
-      let nx = cx + dx[i];
-      let ny = cy + dy[i];
-
-      if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
-      if (arr[nx][ny] === 0) continue;
-      if (visited[nx][ny] === true) continue;
-      visited[nx][ny] = true;
-      queue.push([nx, ny]);
+const input = require('fs').readFileSync("./dev/stdin").toString().trim().split('\n')
+const T = Number(input.shift());
+const dx = [-1, 1, 0, 0]
+const dy = [0, 0, -1, 1]
+const bfs = (x, y, visited, graph, N, M) => {
+        const queue = [];
+        queue.push([x, y])
+        visited[x][y] = true
+        while(queue.length > 0) {
+            const [cx, cy] = queue.pop();
+            for(let i = 0; i < 4; i++) {
+                let nx = cx + dx[i];
+                let ny = cy + dy[i];
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if(visited[nx][ny]) continue;
+                if(graph[nx][ny] === 0) continue;
+                visited[nx][ny] = true
+                queue.push([nx, ny])
+            }
+        }
+        
     }
-  }
-};
 
-for (let i = 0; i < N; i++) {
-  let answer = 0;
-  const [M, N, K] = input.shift().split(" ").map(Number);
-  const arr = Array.from({ length: N }, () => new Array(M).fill(0));
-  const visited = Array.from({ length: N }, () => new Array(M).fill(false));
-  for (let i = 0; i < K; i++) {
-    const [X, Y] = input.shift().split(" ").map(Number);
-    arr[Y][X] = 1;
-  }
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (arr[i][j] === 1 && visited[i][j] === false) {
-        BFS(i, j, arr, visited, N, M);
-        answer++;
-      }
+for(let i = 0; i < T; i++) {
+    const [N, M, K] = input.shift().split(' ').map(Number);
+    const visited = Array.from({length: N}, () => new Array(M).fill(false))
+    const arr = Array.from({length: N}, () => new Array(M).fill(0))
+    let answer = 0;
+    for(let i = 0; i < K; i++) {
+        const [X, Y] = input.shift().split(' ').map(Number);
+        arr[X][Y] = 1
     }
-  }
-  console.log(answer);
+    for(let i = 0; i < N; i++) {
+        for(let j  = 0; j < M; j++) {
+            if(!visited[i][j] && arr[i][j] === 1) {
+                bfs(i,j,visited,arr,N, M) 
+                answer++
+            }
+        }
+    }
+    console.log(answer)
 }
