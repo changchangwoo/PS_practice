0e13ca2fc49006f44858523dc28252a354f0a693
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-11-25 20:09:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 3264 ms, Memory: 20204 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,35 +1,32 @@
-let input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split(" ");
-input = input.map(Number);
-let s = input[0];
-let t = input[1];
-let v = new Array(100001).fill(false);
+const input = require('fs').readFileSync("./dev/stdin").toString().trim().split('\n')
+const [N, K] = input.shift().split(' ').map(Number)
+const LIMIT = 100000
+const visited = new Array(LIMIT).fill(false)
+const bfs = (v) => {
+    const queue = []
+    queue.push([v, 0])
+    visited[v] = true
+    while (queue.length > 0) {
 
-function bfs(start, visited, target) {
-  const queue = [];
-  queue.push([start, 0]);
+        let [cv, count] = queue.shift()
+        if (cv === K) {
+            console.log(count)
+            break;
+        }
+        if (cv - 1 >= 0 && !visited[cv - 1]) {
+            queue.push([cv - 1, count + 1])
+            visited[cv - 1] = true
+        }
+        if (cv + 1 <= LIMIT && !visited[cv + 1]) {
+            queue.push([cv + 1, count + 1])
+            visited[cv + 1] = true
+        }
 
-  const enqueue = (current, count) => {
-    if (current >= 0 && current <= 100000 && !visited[current]) {
-      queue.push([current, count]);
-      visited[current] = true;
+        if (cv * 2 <= LIMIT && !visited[cv * 2]) {
+            queue.push([cv * 2, count + 1])
+            visited[cv * 2] = true
+        }
     }
-  };
-
-  while (queue.length > 0) {
-    const [current, count] = queue.shift();
-    if (current === target) {
-      console.log(count);
-      return;
-    }
-
-    enqueue(current - 1, count + 1);
-    enqueue(current + 1, count + 1);
-    enqueue(current * 2, count + 1);
-  }
 }
 
-bfs(s, v, t);
+bfs(N)
