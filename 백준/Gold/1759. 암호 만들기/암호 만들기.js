81ec52a692122671c4096e0025bf5fa97207497d
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-12-05 17:27:57 +0900

    [Gold V] Title: 암호 만들기, Time: 152 ms, Memory: 9968 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.js
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.js
@@ -1,71 +1,50 @@
-// 조건1. 서로 다른 알파벳 L개 집합
-// 조건2. 정렬된 문자
-// 조건3. 최소 한 개의 모음
-// 조건4. 최소 두 개의 자음
-// 조건5. 중복 제거
-// 제한 시간은 2초 넉넉하이
+/*
+조건 1. 정렬
+조건 2. 최소 한 개의 모음과 두 개의 자음
+백트래킹으로 탐색
+*/
+let input = require('fs').readFileSync("./dev/stdin").toString().trim().split('\n')
+const jaum = ["b", "c", "d", "f", "g", "h", "j", "k", 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'x', 'z', 'w', 'y']
+const moum = ["a", "e", "i", 'o', 'u']
+const [N, M] = input.shift().split(' ').map(Number)
+const arr = input.shift().split(' ').sort();
+const result = []
+const answers = []
 
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
 
-const vowel = ["a", "e", "i", "o", "u"];
-const consonant = [
-  "b",
-  "c",
-  "d",
-  "f",
-  "g",
-  "h",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
 
-const [L, C] = input.shift().split(" ").map(Number);
-const alphabets = input.shift().split(" ").sort();
-const result = [];
-const answer = [];
-const visited = Array(C + 1).fill(false);
-function backtracking(num) {
-  if (result.length === L) {
-    if (checkingAlpha(result)) answer.push(result.join(""));
-    return;
-  }
-  for (let i = 0; i < alphabets.length; i++) {
-    if (!visited[i] && i >= num) {
-      visited[i] = true;
-      result.push(alphabets[i]);
-      backtracking(i);
-      visited[i] = false;
-      result.pop();
+const isCheckJaum = (result_arr) => {
+    let count = 0;
+    for (let i = 0; i < result_arr.length; i++) {
+        if (jaum.includes(result_arr[i])) count++;
+        if (count >= 2) return true
     }
-  }
+    return false
 }
-backtracking(0);
 
-function checkingAlpha(arr) {
-  let vCount = 0;
-  let cCount = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (vowel.includes(arr[i])) vCount++;
-    if (consonant.includes(arr[i])) cCount++;
-    if (vCount >= 1 && cCount >= 2) return true;
-  }
-  return false;
+const isCheckMoum = (result_arr) => {
+    let count = 0;
+    for (let i = 0; i < result_arr.length; i++) {
+        if (moum.includes(result_arr[i])) count++;
+        if (count >= 1) return true
+    }
+    return false
 }
-console.log(answer.join("\n"));
+
+
+const dfs = (v) => {
+    if (result.length === N) {
+        if (isCheckJaum(result) && isCheckMoum(result)) {
+            answers.push(result.join(''))
+        }
+        return
+    }
+    for (let i = v; i < M; i++) {
+        result.push(arr[i]);
+        dfs(i + 1)
+        result.pop(arr[i]);
+    }
+}
+dfs(0)
+console.log(answers.join('\n'))
+
