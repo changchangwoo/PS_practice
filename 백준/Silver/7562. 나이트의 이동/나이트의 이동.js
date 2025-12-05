c7ea1f37660223ab6ec927216fecbc1b95683665
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-12-05 16:35:01 +0900

    [Silver I] Title: 나이트의 이동, Time: 428 ms, Memory: 30196 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.js
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.js
@@ -1,37 +1,44 @@
-let input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n')
-let N = Number(input.shift())
+const input = require('fs').readFileSync("./dev/stdin").toString().trim().split("\n")
+const T = Number(input.shift())
 
-for (let i = 0; i < N; i++) {
-    let size = Number(input.shift())
-    let [x, y] = input.shift().split(' ').map(Number)
-    let [targetX, targetY] = input.shift().split(' ').map(Number)
-    let checked = Array.from({ length: size }, () => new Array(size).fill(0))
-    let visited = Array.from({ length: size }, () => new Array(size).fill(false))
-    const queue = []
+const dx = [-2, -1, +1, +2, +2, +1, -1, -2];
+const dy = [+1, +2, +2, +1, -1, -2, -2, -1];
+for (let i = 0; i < T; i++) {
+    const I = Number(input.shift());
+    const visited = Array.from({ length: I }, () => new Array(I).fill(false));
+    const [x, y] = input.shift().split(' ').map(Number);
+    const [tx, ty] = input.shift().split(' ').map(Number);
 
-    let dy = [-2, -2, -1, -1, +1, +1, +2, +2]
-    let dx = [+1, -1, +2, -2, +2, -2, +1, -1]
 
-    function bfs(startX, startY) {
-        queue.push([startX, startY])
-        visited[startX][startY] = true
+    const bfs = (x, y) => {
+        const queue = [];
+        let flag = 0
+        queue.push([x, y, 0])
+        if (x === tx && y === ty) {
+            console.log(0)
+            return
+        }
+        visited[x][y] = true
         while (queue.length > 0) {
-            let [x, y] = queue.shift()
-            visited[x][y] = true
-            if (x === targetX && y === targetY) break
+
+            flag++;
+            const [cx, cy, count] = queue.shift();
             for (let i = 0; i < 8; i++) {
-                let nx = dx[i] + x
-                let ny = dy[i] + y
-                if (nx >= 0 && nx < size && ny >= 0 && ny < size && !visited[nx][ny]) {
-                    visited[nx][ny] = true
-                    checked[nx][ny] = checked[x][y] + 1
-                    queue.push([nx,ny])
+                let nx = cx + dx[i];
+                let ny = cy + dy[i];
+
+                if (nx < 0 || nx >= I || ny < 0 || ny >= I) continue;
+                if (visited[nx][ny]) continue;
+                if (nx === tx && ny === ty) {
+                    console.log(count + 1)
+                    return
                 }
+                visited[nx][ny] = true
+                queue.push([nx, ny, count + 1])
             }
+
         }
     }
 
     bfs(x, y)
-
-    console.log(checked[targetX][targetY])
 }
