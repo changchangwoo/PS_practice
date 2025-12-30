8dde34e76b5bb34f59cf79484d9188e2dac0a6bc
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2025-12-30 12:02:19 +0900

    [level 2] Title: 구명보트, Time: 34.40 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,20 +1,20 @@
 /*
-최대 2명씩
-가장 작은 사람은 가장 큰 사람이랑 타야한다
-*/
+구명보트 최대한 적게 사용 => 가장 몸무게 적은 사람과 많은 사람이 탑승
+몸무게가 초과되는 경우, 뒤 인덱스를 한칸 올리면서 비교 => count + 1
+정상 범위의 경우, 앞 인덱스를 올리고 +1, pop();
+50 50
 
+*/
 function solution(people, limit) {
-    people = people.sort((a,b) => a-b)
-    let count = 0
-    while(people.length > 0) {
-        if(people[0] + people[people.length-1] > limit) {
-            people.pop()
-            count++;
-        } else {
-            people.shift()
-            people.pop()
-            count++;
-        }
+    var answer = 0;
+    const sorted_people = people.sort((a, b) => a - b);
+    let startIdx = 0;
+    while(startIdx < sorted_people.length) {
+        let start = sorted_people[startIdx] 
+        let end = sorted_people[sorted_people.length - 1]
+        if(start + end <= limit) startIdx++;
+        answer++;
+        sorted_people.pop();
     }
-    return count
+    return answer;
 }
