3dd87fbaf3541d3deb86555df705494477b7fe06
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-01 19:37:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.29 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,14 @@
 function solution(participant, completion) {
-    let answer
-    let hash = new Map()
-    participant.forEach(element => {
-        if (hash.get(element) === undefined) {
-            hash.set(element, 0)
-        }
-        else hash.set(element, hash.get(element)+1)
-    });
-
-    completion.forEach(element => {
-        if(hash.get(element) != 0) hash.set(element, hash.get(element)-1)
-        else if (hash.get(element) === 0) hash.delete(element)
-    });
-
-    for(let [key, value] of hash) {
-        answer = key
+    const hash = new Map();
+    const answer = []
+    for(let name of participant) {
+        hash.set(name, hash.get(name) ? hash.get(name) + 1 : 1)
     }
-
-    return answer
+    for(let name of completion) {
+        hash.set(name, hash.get(name) ? hash.get(name) - 1 : 0)
+    }
+    for(const [key, value] of hash.entries()) {
+        if(value > 0) return key
+    }
+    
 }
