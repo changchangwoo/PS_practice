c80fff8f38de244915c3b4444c173bbb5c3d0128
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-02 13:43:37 +0900

    [level 2] Title: 의상, Time: 0.29 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,18 +1,14 @@
 function solution(clothes) {
-    var answer = 0;
-    let map = new Map()
-    clothes.forEach(element => {
-        arr = []
-        arr.push(element[0])
-        if (map.has(element[1])) {
-            arr = map.get(element[1])
-            arr.push(element[0])
-            map.set(element[1], arr)
-        } else (map.set(element[1], arr))
-    });
-    let sum = 1
-    map.forEach((value, key) => {
-        sum = sum * (value.length+1)
-    });
-    return sum-1
+    const hash = new Map();
+    for(cloth of clothes) {
+        const [name, category] = cloth
+        hash.set(category, hash.get(category) ? [...hash.get(category), name] : [name])
+    }
+    
+    let count = 1
+    for([key, value] of hash) {
+        count *= value.length+1
+    } 
+    
+    return count-1;
 }
