a4773d2d42f75c99e24fe64721d1898b5888906c
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-02 14:21:22 +0900

    [level 3] Title: 베스트앨범, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,42 +1,34 @@
+/*
+장르별로 가장 많이 재생된 노래를 두 개씩 모아 배스트 앨범
+1. 속한 노래가 많이 재생된 장르 수록
+2. 장르 내에서 많이 재생된 노래 수록
+3. 장르 내에서 재생힛수가 같은 경우, 고유 번호 낮은 노래 수록
+*/
 function solution(genres, plays) {
     var answer = [];
-    play_count = 0
-    let sort = new Map()
-    let sort_arr = []
-    for (let i = 0; i < genres.length; i++) {
-        if (sort.has(genres[i])) {
-            sum = sort.get(genres[i]) + plays[i]
-            sort.set(genres[i], sum)
-        }
-        else {
-            sort.set(genres[i], plays[i])
+    const hash = new Map();
+    for(let i = 0; i < plays.length; i++) {
+        if(!hash.has(genres[i])) {
+            hash.set(genres[i], [{id : i, plays : plays[i]}])
+        } else {
+            hash.set(genres[i], [...hash.get(genres[i]), {id : i, plays : plays[i]}])
         }
     }
-    sort.forEach((value, key) => {
-        sort_arr.push({ key: key, value: value })
-    });
-    sort_arr = sort_arr.sort((a, b) => b.value - a.value)
-    let hash = new Map()
-    for (let i = 0; i < genres.length; i++) {
-        if (hash.has(genres[i])) {
-            obj = { no: i, count: plays[i] }
-            arr = hash.get(genres[i])
-            arr.push(obj)
-            hash.set(genres[i], arr)
+    const arr = []
+    for(const [key, value] of hash) {
+        let count = 0;
+        for(d of hash.get(key)) {
+            count += d.plays;
         }
-        else {
-            obj = { no: i, count: plays[i] }
-            hash.set(genres[i], [obj])
-        }
+        arr.push({genre : key, total : count})
     }
-    for (let i = 0; i < sort_arr.length; i++) {
-        hash_data = hash.get(sort_arr[i].key)
-        hash_data.sort((a, b) => b.count - a.count)
-        if (hash_data.length < 2) { answer.push(hash_data[0].no) }
-        else if (hash_data.length >= 2) {
-            answer.push(hash_data[0].no)
-            answer.push(hash_data[1].no)
-        }
+    const sorted_arr = arr.sort((a, b) => b.total - a.total)
+    for(const item of sorted_arr) {
+        const inArr = hash.get(item.genre).sort((a, b) => {
+            if(a.plays === b.plays) return a.id - b.id
+            return b.plays - a.plays
+        })
+        inArr.slice(0, 2).forEach((v) => answer.push(v.id))
     }
     return answer;
 }
