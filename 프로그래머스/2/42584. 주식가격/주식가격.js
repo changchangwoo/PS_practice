c1f5f68ba8b062cf1997e7aa2ddbe45a0f600dea
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-03 00:31:38 +0900

    [level 2] Title: 주식가격, Time: 31.42 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,12 +1,13 @@
 function solution(prices) {
-    let answer = []
-    for(let i = 0; i < prices.length; i++) {
-        let count = 0
-        for(let j =i+1; j < prices.length; j++) {
-            count +=1
-            if (prices[i] > prices[j]) { break; }
+    var answer = [];
+    let stack = [];
+    for (let i = 0; i < prices.length; i++) {
+        let c = 0
+        for(let j = i+1; j < prices.length; j++) {
+            c++;
+            if(prices[i] > prices[j]) break;
         }
-        answer.push(count)
+        stack.push(c)
     }
-    return answer
+    return stack;
 }
