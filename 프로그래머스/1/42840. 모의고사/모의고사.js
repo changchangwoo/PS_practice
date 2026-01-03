ccb2f1929b9c4627b536d67a44e655f46c985174
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-04 00:08:09 +0900

    [level 1] Title: 모의고사, Time: 4.52 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,20 +1,18 @@
 function solution(answers) {
-    let result = []
-    let num_1 = [1, 2, 3, 4, 5]
-    let num_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    let num_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-
-    let num_count = [{ no: 1, count: 0 }, { no: 2, count: 0 }, { no: 3, count: 0 }]
-
-    for (let i = 0; i < answers.length; i++) {
-        if (num_1[i % 5] === answers[i]) num_count[0].count++
-        if (num_2[i % 8] === answers[i]) num_count[1].count++
-        if (num_3[i % 10] === answers[i]) num_count[2].count++
+    const num = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]    
+    const result = [0, 0, 0]
+    const answer = []
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < answers.length; j++) {
+            if(answers[j] === num[i][j % num[i].length]) {
+                result[i]++;
+            }
+        }
     }
-    num_count = num_count.sort((a, b) => b.count - a.count)
-    num_count.forEach(element => {
-        if (element.count === num_count[0].count) result.push(element.no)
-    });
-    return result
-
+    max = Math.max(...result)
+    result.forEach((v, idx) => {
+        if(v === max) answer.push(idx+1)
+    })
+        
+    return answer;
 }
