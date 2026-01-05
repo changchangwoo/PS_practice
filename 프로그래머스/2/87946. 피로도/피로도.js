0346a9eedc9c70241445515107a5b6886a852938
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-05 13:04:15 +0900

    [level 2] Title: 피로도, Time: 5.19 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,17 +1,17 @@
 function solution(k, dungeons) {
-    let visited = new Array(dungeons.length).fill(false)
-    let result = []
-    function backtracking(staminar, count) {
-        for (let i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && staminar >= dungeons[i][0]) {
+    var answer = -1;
+    const visited = new Array(dungeons.length+1).fill(false);
+    const dfs = (stamina, count) => {
+        answer = Math.max(answer, count)
+        for(let i = 0; i < dungeons.length; i++) {
+            if(!visited[i] && stamina >= dungeons[i][0]) {
                 visited[i] = true
-                backtracking(staminar-dungeons[i][1], count + 1)
+                dfs(stamina - dungeons[i][1], count+1)
                 visited[i] = false
             }
         }
-        result.push(count)
+        return
     }
-    backtracking(k, 0)
-    console.log(result)
-    return Math.max(...result)
+    dfs(k, 0)
+    return answer;
 }
