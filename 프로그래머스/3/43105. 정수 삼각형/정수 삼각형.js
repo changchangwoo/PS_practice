891d9b627c182cdfe8f9734c68be088b3b88f405
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-05 19:28:54 +0900

    [level 3] Title: 정수 삼각형, Time: 8.16 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,17 +1,24 @@
 /*
-맨 좌측 = DP[i-1][0] + DP[i][0]
-맨 우측 = DP[i-1][LEN-1] + DP[i][LEN]
-나머지 = Math.max(DP[i-1][LEN-1], DP[i-1][LEN])
+
+DP[2][0] = DP[1][0] + k
+DP[2][1] = Max(DP[1][0], DP[1][1]) + k
+DP[2][2] = DP[1][2] + k
+
 */
+
 function solution(triangle) {
-    triangle.forEach((arr, idx) => {
-        if(idx < 1) return
-        let len = arr.length
-        for(let i = 0; i < len; i++) {
-            if(i === 0) triangle[idx][0] = triangle[idx][0] + triangle[idx-1][0]
-            else if(i === len-1) triangle[idx][len-1] = triangle[idx][len-1] + triangle[idx-1][len-2]
-            else triangle[idx][i] = Math.max(triangle[idx-1][i-1], triangle[idx-1][i]) + triangle[idx][i]
+    var answer = 0;
+    const len = triangle.length;
+    const DP = Array.from({length : len}, () => new Array())
+    DP[0][0] = triangle[0][0]
+    DP[1][0] = DP[0][0] + triangle[1][0]
+    DP[1][1] = DP[0][0] + triangle[1][1]
+    for(let i = 2; i < len; i++) {
+        DP[i][0] = DP[i-1][0] + triangle[i][0]
+        DP[i][i] = DP[i-1][i-1] + triangle[i][i]
+        for(let j = 1; j < triangle[i].length - 1; j++) {
+            DP[i][j] = Math.max(DP[i-1][j-1], DP[i-1][j]) + triangle[i][j]
         }
-    });
-    return Math.max(...triangle[triangle.length-1]);
+    }
+    return Math.max(...DP[len-1])
 }
