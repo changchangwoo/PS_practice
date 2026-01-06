89d62f5a6ae1d38ba73df85af2042bd301ac430a
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-06 18:20:18 +0900

    [level 3] Title: 여행경로, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,24 +1,48 @@
+/*
+항상 ICN 출발, 주어진 항공권을 모두 사용해야한다.
+*/
 function solution(tickets) {
-    result = []
-    tickets = tickets.sort()
-    visited = new Array(tickets.length).fill(false)
-    function dfs(str, count) {
-        result.push(str)
-        if(count === tickets.length) {
-            answer = result
-            return true
+    var answer = ["ICN"];
+    const hash = new Map();
+    for(const [start, end] of tickets) {
+        if(hash.has(start)) {
+            let arr = hash.get(start)
+            arr.push(end)
+            hash.set(start, arr.sort())
+        } else {
+            hash.set(start, [end])
         }
-        for(let i = 0; i <tickets.length; i++){
-            if(!visited[i] && tickets[i][0] === str) {
-                visited[i] = true
-                if(dfs(tickets[i][1], count+1)) return true
-                visited[i] = false
+    }
+    const visited = new Map();
+    for(const [key, value] of hash) {
+        visited.set(key, new Array(value.length).fill(false))
+    }
+    
+    let found = false
+    let result = []
+    const dfs = (s) => {
+        if(found) return
+        if(answer.length === tickets.length +1) {
+            result = [...answer]
+            found = true
+            return
+        }
+        if(!hash.has(s)) return
+        const arr = hash.get(s);
+        
+        for(let i = 0; i < arr.length; i++) {
+            let visitedTicket = visited.get(s)
+            if(!visitedTicket[i]) {
+                visitedTicket[i] = true
+                answer.push(arr[i])
+                dfs(arr[i])
+                answer.pop()
+                visitedTicket[i] = false
+
             }
         }
-        result.pop()
-        return false
-
     }
-    dfs('ICN', 0)
-    return answer
+    dfs("ICN")
+    
+    return result;
 }
