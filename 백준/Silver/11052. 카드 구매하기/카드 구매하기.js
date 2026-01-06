cebb9ef76a54edf42dcd5c202e7a0e85d93a068f
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-06 14:21:31 +0900

    [Silver I] Title: 카드 구매하기, Time: 160 ms, Memory: 10448 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
@@ -1,49 +1,34 @@
 /*
-반복문을 통해 0개 까지의 값을 미리 선언
+카드팩 종류 = 1..N개 까지의 포함된 카드팩
+민규는 최대한 돈을 써서 카드 N개 구매, 카드 i 포함된 카드팩 가격 pi
+카드팩에 포함된 카드 개수의 합은 N과 같아야 한다
+카드 N번을 사는 횟수
+=> N의 쌍의 합?
+4
+1, 3
+2, 2
+DP[1], DP[3]
 
-카드개수는 피보나치 규칙과 일치
-DP[i] = DP[i-1] + DP[i-2] // 카드 개수의 조건
-DP[i] = Math.max(arr[i], DP[i-1] + DP[i-2])
+2, 0
+1, 1
 
-3개 사는데 
-
-1 5
-2 4
-3 3
-DP[1] = arr[1]
-DP[2] = math.max(arr[1]+arr[1], arr[2])
-DP[3] = math.max(arr[3], DP[2]+DP[1])
-DP[4] = DP[3] + DP[1], 
-DP[6]
-Dp[5]+DP[1],
 
 */
 
-const input = require("fs")
-  .readFileSync("./dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
+const input = require('fs').readFileSync("./dev/stdin").toString().trim().split('\n')
+const N = +input.shift()
+const arr = input.shift().split(' ').map(Number)
+const DP = new Array(N).fill(-Infinity)
 
-const N = Number(input.shift());
-const arr = input.shift().split(" ").map(Number);
-arr.unshift(0);
-const DP = new Array(N + 1).fill(0);
+DP[0] = arr[0]
+DP[1] = Math.max(arr[1], DP[0] * 2);
+for (let i = 2; i < arr.length; i++) {
+    let max = -Infinity
+    for (let j = 0; j < i; j++) {
+        max = Math.max(max, DP[i - 1 - j] + DP[j])
 
-DP[1] = arr[1];
-DP[2] = Math.max(arr[2], arr[1] * 2);
-DP[3] = Math.max(DP[2] + DP[1], arr[3]);
-
-for (let i = 4; i <= N; i++) {
-  let idx = 1;
-  let edx = i - 1;
-  const stack = [];
-  while (idx <= edx) {
-    stack.push(DP[idx] + DP[edx]);
-    idx++;
-    edx--;
-  }
-  DP[i] = Math.max(...stack, arr[i]);
+    }
+    DP[i] = Math.max(arr[i], max)
 }
+console.log(DP[N - 1])
 
-console.log(DP[N]);
