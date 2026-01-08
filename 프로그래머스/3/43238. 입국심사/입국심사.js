b77b43df978a0b20ac6669c2cd2de8f7bd3e312a
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-01-08 18:47:39 +0900

    [level 3] Title: 입국심사, Time: 32.33 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,5 +1,4 @@
 function solution(n, times) {
-    var answer = 0;
     let left = 1;
     let right = Math.max(...times) * n;
     while(left <= right) {
@@ -13,8 +12,7 @@ function solution(n, times) {
             right = mid - 1
         } else {
             left = mid + 1
-            answer = left
         }
     }
-    return answer;
+    return left;
 }
