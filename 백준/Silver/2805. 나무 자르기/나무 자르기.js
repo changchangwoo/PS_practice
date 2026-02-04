169c6e1db8e239c9b28dba49e566b88ed0487f23
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-02-04 17:02:21 +0900

    [Silver II] Title: 나무 자르기, Time: 904 ms, Memory: 132616 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -1,31 +1,24 @@
-const input = require('fs').readFileSync("/dev/stdin").toString().trim().split('\n')
+const input = require('fs').readFileSync("./dev/stdin").toString().trim().split("\n")
 const [N, M] = input.shift().split(' ').map(Number)
-let woods = input.shift().split(' ').map(Number)
-woods = woods.sort((a, b) => a - b)
+const arr = input.shift().split(' ').map(Number).sort((a, b) => a - b);
+let left = 0
+let right = arr[arr.length - 1]
+/*
 
-const checkWood = (max) => {
-    let sum = 0
-    for (let i = 0; i < woods.length; i++) {
-        if (woods[i] - max > 0) sum += (woods[i] - max)
+*/
+let answer = 0
+while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    let sum = 0;
+    for (let i = 0; i < N; i++) {
+        if (arr[i] >= mid) sum += arr[i] - mid
     }
-    return sum
-}
-
-const binaraySearch = (max) => {
-    low = 0
-    high = max
-    while (low <= high) {
-        mid = Math.floor((low + high) / 2)
-        let wood = checkWood(mid)
-        if (wood >= M) {
-            low = mid + 1
-        } else if (wood < M) {
-            high = mid - 1
-        }
+    if (sum < M) {
+        right = mid - 1;
+    } else {
+        answer = mid
+        left = mid + 1;
     }
-    return high
 
 }
-
-console.log(binaraySearch(woods[woods.length - 1]))
-
+console.log(answer)
