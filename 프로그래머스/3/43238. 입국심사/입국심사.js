65dc65bff1db1fdd2bbb74a7d8684cba58a77347
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-02-04 18:49:27 +0900

    [level 3] Title: 입국심사, Time: 38.19 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,18 +1,27 @@
+/*
+tic을 나누어서 n이 되는 경우가 가장 최소 일 때를 구함
+최대 걸리는 시간은 51초로 임의 가정
+Math.floor((tic / time)) = sum
+이 sum이 6보다 작은 경ㅇ
+*/
+
 function solution(n, times) {
-    let left = 1;
-    let right = Math.max(...times) * n;
+    var answer = 0;
+    let left = 0;
+    let right = Math.max(...times) * Math.floor(n / times.length)
     while(left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        
-        let count = 0;
-        for (const time of times) {
-            count += Math.floor(mid / time)
+        const mid = Math.floor((left + right) / 2)
+        let sum = 0;
+        for(const time of times) {
+            sum += Math.floor(mid/time)
         }
-        if(count >= n) {
-            right = mid - 1
-        } else {
+        if(sum < n) {
             left = mid + 1
+        } else {
+            answer = mid
+            right = mid - 1
         }
+            
     }
-    return left;
+    return answer;
 }
