e1c7157303ecc1809765e1c91004ebfb7897cd7f
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-02-04 12:24:52 +0900

    [level 1] Title: 체육복, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,36 +1,34 @@
+/*
+바로 앞 번호 학생이나, 뒷 번호 학생에게만 체육복을 빌려줄 수 있다.
+학생 수 30명 이하 시간복잡도 X
+여벌 체육복을 가져온 학생이 체육복을 도난 당한 경우 제외
+
+1, 2, 4
+
+2, 3, 4, 5
+
+
+
+*/
 function solution(n, lost, reserve) {
-    let count = 0;
-    let idx = [];
-    lost = lost.sort((a,b)=> a-b)
-    reserve = reserve.sort((a,b)=> a-b)
 
-
-    // 일치하는 쌍을 찾아 표시
-    for (let i = 0; i < lost.length; i++) {
-        for (let j = 0; j < reserve.length; j++) {
-            if (lost[i] === reserve[j]) {
-                reserve.splice(j, 1);
-                idx.push(i);
-                break; // 일치하는 경우에는 루프를 빠져나옴
-            }
+    const newLost = lost.filter((v) => !reserve.includes(v)).sort((a, b) => a - b)
+    const newReserve = reserve.filter((v) => !lost.includes(v)).sort((a, b) => a - b)
+    var answer = 0;
+    const hash = new Map();
+    newLost.forEach((v) => hash.set(v, true))
+    for(const item of newReserve) {
+        if(hash.get(item)) continue;
+        if(hash.get(item-1)) {
+            hash.delete(item-1)
+            continue;
+        }
+        if(hash.get(item+1)) {
+            hash.delete(item+1)
+            continue;
         }
     }
-
-    // 일치한 쌍의 인덱스를 기준으로 lost 배열에서 삭제
-    for (let i = idx.length - 1; i >= 0; i--) {
-        lost.splice(idx[i], 1);
-    }
-
-    // 여분의 체육복을 빌려주는 처리
-    lost.forEach(element => {
-        for (let i = 0; i < reserve.length; i++) {
-            if (element === reserve[i] + 1 || element === reserve[i] - 1) {
-                reserve.splice(i, 1);
-                count += 1;
-                break;
-            }
-        }
-    });
-
-    return n - (lost.length - count);
+    return n - [...hash.entries()].length
+    
+    return answer;
 }
