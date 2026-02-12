5605925d53d8f015a7395639dbd31e42af074137
changchangwoo <50562562+changchangwoo@users.noreply.github.com>
2026-02-12 18:15:25 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,26 +1,40 @@
+/*
+한 번의 한 개의 알파벳만 변경할 수 있다.
+words에 있는 단어로만 변환할 수 있다.
+최대 50까지의 수
+BFS로 탐색 => 그러면 중복 체크가 안되는거아님?, 백트래킹?
+1. hit에서 하나 바꿨을 때, 바꿀 수 있는 단어들 전부 탐색해서 안됨, 그러면 추적이 불가능함
+2. dfs로 백트래킹해야함.
+*/
 function solution(begin, target, words) {
-    var answer = 0;
-    return answer;
-}function solution(begin, target, words) {
-    var count = 0;
-    let queue = []
-    let visited = []
-    queue.push([begin, count])
-    while (queue.length != 0) {
-        [q, count] = queue.shift()
-        if(q === target) return count
-        words.forEach(element => {
-            if(visited.includes(element)) return
-            let non_equal = 0
-            for (let i = 0; i < element.length; i++) {
-                if (q[i] !== element[i]) non_equal++
+    var answer = Infinity;
+    const visited = new Array(words.length).fill(false)
+    if(!words.includes(target)) return 0
+        dfs(begin, 0)
+    function dfs(str, count) {
+        if(str === target) {
+            answer = Math.min(answer, count)
+            return;
+        }
+        for(let i = 0; i < words.length; i++) {
+            if(!visited[i] && checkWord(words[i], str)) {
+                visited[i] = true
+                dfs(words[i], count+1)
+                visited[i] = false
             }
-            if (non_equal === 1) {
-                queue.push([element, ++count])
-                visited.push(element)
-            }
-        });
+        }
+        return;
+        
     }
+    
+    return answer === Infinity ? 0 :  answer;
+}
 
-    return 0;
+function checkWord(word, str) {
+    let sum = 0;
+    for(let i = 0; i < word.length; i++) {
+       if(word[i] === str[i]) sum++;
+    }
+    if(sum === str.length-1) return true;
+    return false
 }
